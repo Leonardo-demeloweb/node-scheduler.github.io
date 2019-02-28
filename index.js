f4e8e0f78352ae9437bfd2f92a8dec8ffd2e8142
Leonardo de Melo <demelo01@gmail.com>
2019-02-28 18:56:28 -0300

    scope date/hour implemented for job-scheduler

--- index.js
+++ index.js
@@ -6,32 +6,8 @@ const nodemailer = require("nodemailer");
 
 app = express();
 
-// Obtém a data/hora atual
-const data = new Date();
-// console.log(new Date());
-
-// Guarda cada sessão de data/ hora em uma variável
-const dia     = (data.getDate() < 10 ? '0' : '') + data.getDate();    // 1-31, aqui com formatação para dois dígitos
-const dia_sem = data.getDay();            // 0-6 (zero=domingo)
-const mes     = ((data.getMonth() + 1) < 10 ? '0' : '') + (data.getMonth() + 1); // 0-11 (zero=janeiro), aqui com formatação para dois dígitos
-const ano2    = data.getYear();           // 2 dígitos
-const ano4    = data.getFullYear();       // 4 dígitos
-const hora    = data.getHours();          // 0-23
-const min     = data.getMinutes();        // 0-59
-const seg     = data.getSeconds();        // 0-59
-//const mseg    = data.getMilliseconds();   // 0-999
-//const tz      = data.getTimezoneOffset(); // em minutos 
-
-// Formata a data e a hora
-const hoje = dia + '-' + mes + '-' + (ano2-100);
-const horas = hora + ':' + min + ':' + seg;
-//console.log(dia);
-
 //log com a confirmação que o serviço está rodando
-console.log('Serviço em funcionamento')
-
-// Mostra o resultado referente ao dia atual com dois dígitos
-console.log('Hoje é ' + hoje);
+console.log('Serviço de agendamento de tarefa iniciado com sucesso em: ' + new Date())
 
 //configurando o email do emissor
 /* const transporter = nodemailer.createTransport({
@@ -42,17 +18,40 @@ console.log('Hoje é ' + hoje);
     }
   }); */
 
-//Url para realização da requisição dinâmica
-//const urlR = 'http://redenacionalderadio.com.br/programas/a-voz-do-brasil-completa/'+`${hoje}`+'-voz-do-brasil.mp3/@@download/file/'+`${hoje}`+'-%20-%20VOZ%20DO%20BRASIL.mp3';
-const url = 'http://redenacionalderadio.com.br/programas/a-voz-do-brasil-completa/15-01-19-voz-do-brasil.mp3/@@download/file/15-01-19-%20-%20VOZ%20DO%20BRASIL.mp3';
-
-//console.log(urlR);
-
 //função periódica para execução da tarefa programada
-const job = new CronJob('00 22 16 * * *', () => {
+const job = new CronJob('00 59 20 * * *', () => {
+    // Obtém a data/hora do dia que o download está sendo realizado
+    let data = new Date();
+    
+    // console.log(new Date());
+
+    // Guarda cada sessão de data/ hora em uma variável
+    let dia     = (data.getDate() < 10 ? '0' : '') + data.getDate();    // 1-31, aqui com formatação para dois dígitos
+    let dia_sem = data.getDay();            // 0-6 (zero=domingo)
+    let mes     = ((data.getMonth() + 1) < 10 ? '0' : '') + (data.getMonth() + 1); // 0-11 (zero=janeiro), aqui com formatação para dois dígitos
+    let ano2    = data.getYear();           // 2 dígitos
+    let ano4    = data.getFullYear();       // 4 dígitos
+    let hora    = data.getHours();          // 0-23
+    let min     = data.getMinutes();        // 0-59
+    let seg     = data.getSeconds();        // 0-59
+    //const mseg    = data.getMilliseconds();   // 0-999
+    //const tz      = data.getTimezoneOffset(); // em minutos 
+
+    // Formata data e hora
+    let hoje = dia + '-' + mes + '-' + (ano2-100);
+    //let hoje = "17-01-19";
+    let horas = hora + ':' + min + ':' + seg;
+    //console.log(dia);
+
+    //log com o dia da realização do download
+    console.log('Log com o dia atual da realização do download ', hoje)    
+    
+    //let url = 'http://redenacionalderadio.com.br/programas/a-voz-do-brasil-completa/15-01-19-voz-do-brasil.mp3/@@download/file/15-01-19-%20-%20VOZ%20DO%20BRASIL.mp3';
+    let url = 'http://redenacionalderadio.com.br/programas/a-voz-do-brasil-completa/'+`${hoje}`+'-voz-do-brasil.mp3/@@download/file/'+`${hoje}`+'-%20-%20VOZ%20DO%20BRASIL.mp3';
+    
     request(url).pipe(fs.createWriteStream('Voz do Brasil.mp3'));
-    console.log('Download iniciado', hoje, 'às', horas );
-    //console.log(url);
+    console.log('Download está sendo iniciado', hoje, 'às', horas );
+    console.log(url);
 
     //enviando email de confirmação de download
     /* const mailOptions = {
@@ -71,7 +70,6 @@ const job = new CronJob('00 22 16 * * *', () => {
       }); */
 
 }, null, true, 'America/Sao_Paulo') 
-
 
 
 
